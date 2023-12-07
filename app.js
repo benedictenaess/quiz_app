e7ba861e2da3a303666448a67a519322f87bc708
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2023-12-07 11:13:45 +0100

    Questions

---
+++ app.js
@@ -0,0 +1,52 @@
+const quizInfo = [
+	{
+	  question:
+		"Which method in JavaScript removes the last element from an array?",
+	  optionA: "unshift()",
+	  optionB: "remove()",
+	  optionC: "pop()",
+	  optionD: "push()",
+	  correctAnswer: "option3",
+	},
+	{
+	  question: "What's the scope of variables declared using const?",
+	  optionA: "function scope",
+	  optionB: "global scope",
+	  optionC: "block scope",
+	  optionD: "inline scope",
+	  correctAnswer: "option3",
+	},
+	{
+	  question:
+		"Which attribute from the following must have a unique value in HTML?",
+	  optionA: "class",
+	  optionB: "name",
+	  optionC: "title",
+	  optionD: "id",
+	  correctAnswer: "option4",
+	},
+	{
+	  question: "Which option is not a part of the CSS box model?",
+	  optionA: "margin",
+	  optionB: "size",
+	  optionC: "padding",
+	  optionD: "border",
+	  correctAnswer: "option2",
+	},
+	{
+	  question: "What is the type of an array in JavaScript?",
+	  optionA: "null",
+	  optionB: "undefined",
+	  optionC: "array",
+	  optionD: "object",
+	  correctAnswer: "option4",
+	},
+	{
+	  question: "Whhich method is used to combine two or more strings?",
+	  optionA: "append()",
+	  optionB: "concat()",
+	  optionC: "typeof()",
+	  optionD: "attach()",
+	  correctAnswer: "option2",
+	},
+  ];
